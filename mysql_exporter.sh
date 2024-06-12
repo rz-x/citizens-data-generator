951fd189b6d5eb58c462321d4e06b63229ad1d88
radon <radekzdonczyk@gmail.com>
2024-06-12 16:52:22 +0200

    mysql_exporter.sh minor changes

--- mysql_exporter.sh
+++ mysql_exporter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # config
-HOST="localhost"
-DB_NAME="your_database_name"
-USER="your_username"
-PASSWORD="your_password"
+HOST="mysql_host"
+DB_NAME="database_name"
+USER="mysql_username"
+PASSWORD="mysql_passwd"
 
 DUMP_FILE="fake_citizen_data.dump"
 
